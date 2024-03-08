57304f6abe5dbc03f17fba7452d316e91d25e0bd
Rohit-Gupta31 <imrgrohit@gmail.com>
2024-03-08 07:01:16 +0300

    Add subscription model
    Signed-off-by: Rohit-Gupta31 <imrgrohit@gmail.com>

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose"
+import mongoose , {Schema} from "mongoose"
+
+const subscriptionSchema = new Schema({
+    subcscriber : {
+        type : Schema.Types.ObjectId, // one who is subscribing
+        ref : "User"
+    },
+    channel : {
+        type : Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing.
+        ref : "User"
+    }
+},{timestamps})
+
+
+
+export const Subscription = mongoose.model("Subscription" , subscriptionSchema)
