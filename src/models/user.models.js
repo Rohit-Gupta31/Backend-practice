ea0ec94cfe8e4f480d4e04b90923846f51a32db2
Rohit-Gupta31 <imrgrohit@gmail.com>
2024-03-09 20:13:58 +0530

    Add new routes for user controller
    Signed-off-by: Rohit-Gupta31 <imrgrohit@gmail.com>

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -55,6 +55,7 @@ userSchema.pre("save" , async function(next){
     this.password = await bcrypt.hash(this.password, 10)
     next();
 // agar password modify hua hai to hi encrypt karna(kyuki jab bhi save karenge kuch bhi to pre chalega). 
+// save hone se pahle pre wala hook chalega.
 });
 
 // Pre middleware functions are executed one after another, when each middleware calls next.=>(data save karne se pahle encrypt kar do)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router }  from 'express';
-import { loginUser, registerUser , logoutUser , refreshAccessToken} from '../controllers/user.controller.js';
+import { loginUser, 
+    registerUser ,
+    logoutUser , 
+    refreshAccessToken , 
+    changeCurrentPassword,
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar,
+    updateUsercoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory  } from '../controllers/user.controller.js';
 import { upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -26,6 +36,22 @@ router.route("/login").post(loginUser) ;
 // secured routes
 router.route("/logout").post(verifyJWT , logoutUser) ;
 router.route("/refresh-token").post(refreshAccessToken) ;
+
+router.route("/change-password").post(verifyJWT , changeCurrentPassword) ;
+router.route("/current-user").get(verifyJWT, getCurrentUser) ;
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) ;
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar") , updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage") , updateUsercoverImage);
+
+// params me se le rahe hai to dhyan rakhna hai
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile);
+
+router.route("/history").get(verifyJWT , getWatchHistory);
+
+
+
 
 /* router.route("/logout").post(verifyJWT , anotherMiddleware ,  logoutUser) ;
  aise ik aor middleware laga sakte hai named anotherMiddleware*/
