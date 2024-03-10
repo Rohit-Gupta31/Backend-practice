341e5b7bba12a0cab3f983868190ee6d96b07408
Rohit-Gupta31 <imrgrohit@gmail.com>
2024-03-10 12:22:57 +0530

    Add comment , like , playlist and tweet model
    Signed-off-by: Rohit-Gupta31 <imrgrohit@gmail.com>

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.models.js";
+import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req ,_, next)=>{
     // res , ka use nahi hai.
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -43,7 +43,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser) ;
 router.route("/update-account").patch(verifyJWT, updateAccountDetails) ;
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar") , updateUserAvatar);
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage") , updateUsercoverImage);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage") , updateUsercoverImage);
 
 // params me se le rahe hai to dhyan rakhna hai
 router.route("/c/:username").get(verifyJWT , getUserChannelProfile);
